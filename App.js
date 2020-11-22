9a27aa3b3b70b79b5dbcbe09e699cb9a99f23f67
Tiago Valim <tiago.valim@gmail.com>
2020-11-22 20:36:18 -0300

    Eventos

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 // Navigations
 import { createDrawerNavigator } from '@react-navigation/drawer';
@@ -21,10 +21,10 @@ const Logout = ( {navigation} ) => {
   return(
     <View>
       <Text>Deseja realmente sair da aplicação?</Text>
-      <Button onPres={() => {
+      <Button onPress={() => {
         AsyncStorage.removeItem('@jwt');
-        navigation.push("Login");
-      }} title="SAIR" />
+        navigation.push('Login');
+      }} title="SAIR" ></Button>
     </View>
   )
 }
